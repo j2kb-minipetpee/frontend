e9591602ebb965b5a6d0b44a9cd79852dd29d8fe
Inkyu Kim <inkyu0103@gmail.com>
2021-08-03 10:42:59 +0900

    first setting

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+const App = (): ReactElement => (
+  <>
+    <h1>하이하이d</h1>
+  </>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_module/,
+      },
+    ],
+
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
