1e12051dabdbd425af4e08364c31db051e967ae4
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2021-08-16 22:22:35 +0900

    fix: 코드리뷰 사항 반영(오타수정)

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,7 +9,7 @@ export const store = configureStore({
 });
 
 type RootState = ReturnType<typeof store.getState>;
-type ApptDispatch = typeof store.dispatch;
+type AppDispatch = typeof store.dispatch;
 
-export const useAppDispatch = () => useDispatch<ApptDispatch>();
+export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
