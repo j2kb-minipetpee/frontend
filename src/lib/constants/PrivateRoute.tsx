33acd234e8b49725fd4521f24e7947ff32894a0d
Inkyu Kim <inkyu0103@gmail.com>
2021-08-19 11:20:00 +0900

    feat:private route에 isauthorized() 항목 추가

--- src/lib/constants/PrivateRoute.tsx
+++ src/lib/constants/PrivateRoute.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Route, Redirect, RouteProps } from 'react-router-dom';
 import { isLogin } from './isLogin';
+import { isAuthorized } from './isAuthorized';
 
 export type RouteModel = {
   restricted?: boolean;
 } & RouteProps;
 
 export const PrivateRoute = ({ component: ComponentName, ...rest }: RouteModel): any => {
-  return <Route {...rest} render={(props) => (isLogin() ? <ComponentName {...props} /> : <Redirect to="/" />)} />;
+  return <Route {...rest} render={(props) => (isLogin() && isAuthorized() ? <ComponentName {...props} /> : <Redirect to="/" />)} />;
 };
 
-export const PublicRoute = ({ component: ComponentName, restricted, ...rest }: RouteModel): any => {
+export const PublicRoute = ({ component: ComponentName, restricted = false, ...rest }: RouteModel): any => {
   return <Route {...rest} render={(props) => (isLogin() && restricted ? <Redirect to="/" /> : <ComponentName {...props} />)} />;
 };
