ef7328c27122290c1354c2d870dafc9e00f2e035
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2021-08-22 17:33:07 +0900

    feat: auth 관련 레이아웃 추가

---
+++ src/layout/AuthLayout.tsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+import React from 'react';
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+export const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return <AuthLayoutContainer>{children}</AuthLayoutContainer>;
+};
+
+const AuthLayoutContainer = styled.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
