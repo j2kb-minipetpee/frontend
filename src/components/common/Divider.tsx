4849d1f52215cd001e8b05815f306106b3b4c828
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2021-08-25 19:12:50 +0900

    feat: 공통 컴포넌트 추가 및 수정

--- src/components/common/Divider.tsx
+++ src/components/common/Divider.tsx
@@ -4,14 +4,16 @@ import { ColorMap } from '@/lib/constants/color';
 
 interface DividerProps {
   type: 'row' | 'column';
+  color?: string;
+  width?: number;
 }
 
-export const Divider = ({ type }: DividerProps) => {
-  return <DividerContainer type={type} />;
+export const Divider = ({ type, color, width = 1 }: DividerProps) => {
+  return <DividerContainer type={type} color={color} width={width} />;
 };
 
 const DividerContainer = styled.div<DividerProps>`
-  background-color: ${ColorMap.Grey50};
-  width: ${({ type }) => (type === 'column' ? `1px` : '100%')};
-  height: ${({ type }) => (type === 'column' ? `100%` : `1px`)};
+  background-color: ${({ color }) => color || ColorMap.GREY100};
+  width: ${({ type, width }) => (type === 'column' ? `${width}px` : '100%')};
+  height: ${({ type, width }) => (type === 'column' ? `100%` : `${width}px`)};
 `;
