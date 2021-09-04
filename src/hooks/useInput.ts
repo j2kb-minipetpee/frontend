20baf1c1864da7f52bed2cc9009f65361783fc00
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2021-09-04 20:41:54 +0900

    fix: hooks 수정

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,9 +1,9 @@
 import React, { useCallback, useState } from 'react';
 
-export const useInput = (initialState?: string) => {
+export const useInput = <T extends HTMLInputElement | HTMLTextAreaElement>(initialState?: string) => {
   const [value, setValue] = useState(initialState ?? '');
 
-  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+  const onChange = useCallback((e: React.ChangeEvent<T>) => {
     setValue(e.target.value);
   }, []);
 
