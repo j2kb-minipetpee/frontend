b22871a937236403f04927ae6528612f17988bba
Inkyu Kim <inkyu0103@gmail.com>
2021-09-07 00:47:49 +0900

    feat:갤러리 화면 작성

--- src/pages/DetailedGalleryPost.tsx
+++ src/pages/DetailedGalleryPost.tsx
@@ -1,3 +1,34 @@
+import { HomepeeLayout } from '@/layout/HomepeeLayout';
+import styled from '@emotion/styled';
 import React from 'react';
+import profileLogo from '@/assets/images/default_profile_small.png';
 
-export const DetailedBoardPost = () => {};
+export const DetailedGalleryPost = () => {
+  return (
+    <HomepeeLayout>
+      <DetailedGalleryPostContainer>
+        <DetailedGalleryPostHeader>
+          <DetailedGalleryPostProfileWrapper>
+            <DetailedGalleryPostProfileImage src={profileLogo} />
+            <span>초코</span>
+          </DetailedGalleryPostProfileWrapper>
+          <div>제목</div>
+          <div>날짜</div>
+          <div>조회수</div>
+        </DetailedGalleryPostHeader>
+      </DetailedGalleryPostContainer>
+    </HomepeeLayout>
+  );
+};
+
+const DetailedGalleryPostContainer = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+const DetailedGalleryPostProfileWrapper = styled.section``;
+const DetailedGalleryPostProfileImage = styled.img``;
+const DetailedGalleryPostHeader = styled.section`
+  width: 100%;
+  display: flex;
+`;
