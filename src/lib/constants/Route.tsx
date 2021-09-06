03c7c630552a3e083d71b163767ef9b8672e9f9f
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2021-09-07 00:21:17 +0900

    fix: Route 수정

--- src/lib/constants/Route.tsx
+++ src/lib/constants/Route.tsx
@@ -2,19 +2,15 @@ import React from 'react';
 import { Route, Redirect, RouteProps, useParams } from 'react-router-dom';
 
 import { useAuth } from '@/hooks';
-import { useMemo } from 'react';
 
 export type RouteModel = {
   restricted?: boolean;
 } & RouteProps;
 
 export const PrivateRoute = ({ component: ComponentName, ...rest }: RouteModel) => {
-  const { id, homepeeId } = useAuth();
-  const { id: targetHomepeeId } = useParams<{ id: string }>();
+  const { id } = useAuth();
 
-  const isAuthorized = useMemo(() => homepeeId === Number(targetHomepeeId), [homepeeId, targetHomepeeId]);
-
-  return <Route {...rest} render={(props) => (id && isAuthorized ? <ComponentName {...props} /> : <Redirect to="/" />)} />;
+  return <Route {...rest} render={(props) => (id ? <ComponentName {...props} /> : <Redirect to="/" />)} />;
 };
 
 export const PublicRoute = ({ component: ComponentName, restricted = false, ...rest }: RouteModel) => {
