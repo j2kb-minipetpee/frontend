06ca4069190ea43ba1cf2c9c768ad6e13939d26d
geonwoomun <38036598+geonwoomun@users.noreply.github.com>
2021-09-10 01:36:05 +0900

    fix: 방명록 관련 수정

--- src/lib/repository/GuestBookRepository.tsx
+++ src/lib/repository/GuestBookRepository.tsx
@@ -11,11 +11,11 @@ import {
 
 class GuestBookRepository {
   async getGuestBooks({ homepeeId, size = 5, page = 0 }: GetGuestBooksRequest & PagenationRequest): Promise<GetGuestBooksResponse> {
-    return client.get(`/${homepeeId}/guest/guest-Books?size=${size}&page=${page}`);
+    return client.get(`/${homepeeId}/guest/guest-notes?size=${size}&page=${page}`);
   }
 
   async addGuestBook({ homepeeId, memberId, content, visible }: AddGuestBookRequest): Promise<AddGuestBookResponse> {
-    return client.post(`/${homepeeId}/guest/guest-Books`, {
+    return client.post(`/${homepeeId}/guest/guest-notes`, {
       memberId,
       content,
       visible,
@@ -23,11 +23,11 @@ class GuestBookRepository {
   }
 
   async editGuestBook({ homepeeId, guestBookId, memberId, content, visible }: EditGuestBookRequest): Promise<void> {
-    return client.put(`${homepeeId}/guest/guest-Books/${guestBookId}`, { memberId, content, visible });
+    return client.put(`${homepeeId}/guest/guest-notes/${guestBookId}`, { memberId, content, visible });
   }
 
   async deleteGuestBook({ homepeeId, guestBookId }: DeleteGuestBookRequest): Promise<void> {
-    return client.delete(`${homepeeId}/guest/guest-Books/${guestBookId}`);
+    return client.delete(`${homepeeId}/guest/guest-notes/${guestBookId}`);
   }
 }
 export default new GuestBookRepository();
